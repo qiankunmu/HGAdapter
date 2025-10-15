129ce8fe8aa5cc8c201484add23609b7af722f44
Guang Yang <55649333+qiankunmu@users.noreply.github.com>
2025-10-15 22:28:53 +0800

    Update HGAdapterConfig.py

--- models/HGAdapterConfig.py
+++ models/HGAdapterConfig.py
@@ -7,7 +7,6 @@ from adapters import SeqBnConfig
 @dataclass(eq=False)
 class HGAdapterConfig(SeqBnConfig):
     num_heads: int = 8
-    use_hyper: bool = True
     num_edge_types: int = 3
     torch_dtype: str = "float"
     dropout: float = 0.2
