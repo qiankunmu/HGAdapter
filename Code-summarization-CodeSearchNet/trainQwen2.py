d8a4cf8656b904c48316c1c15af589757e7efaf5
Guang Yang <55649333+qiankunmu@users.noreply.github.com>
2025-10-15 22:41:29 +0800

    Update trainQwen2.py

--- Code-summarization-CodeSearchNet/trainQwen2.py
+++ Code-summarization-CodeSearchNet/trainQwen2.py
@@ -103,7 +103,7 @@ def main():
         # Initialize model
         model = MyQwen2HGAdapterForCausalLM.from_pretrained(pretrain_model_name_or_path, torch_dtype=torch.bfloat16, 
                                                             pad_token_id=tokenizer.pad_token_id)
-        adapter_config = HGAdapterConfig(use_hyper=True, reduction_factor=reduction_factor, num_heads=num_heads,
+        adapter_config = HGAdapterConfig(reduction_factor=reduction_factor, num_heads=num_heads,
                                          dropout=dropout_out, torch_dtype="bfloat16")
 
         model.model.add_adapter("code-summarization", config=adapter_config)
@@ -288,3 +288,4 @@ def eval(model, dataloader, tokenizer, device, text_save_path=None):
 
 if __name__ == "__main__":
     main()
+
